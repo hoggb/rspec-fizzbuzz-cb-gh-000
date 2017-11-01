b00f8e082232834cc5a1f09cbd3649bd692f0883
Bret Hogg <hoggb@wwu.edu>
2017-11-01 18:53:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,11 @@
 def fizzbuzz(num)
   if num % 3 == 0
-    fizz = "Fizz"
+    value = "Fizz"
   end
-  elsif
-    num % 5
+  elsif num % 5 == 0
+    value = "Buzz"
+  end
+  elsif num % 5 == 0 && num % 3 == 0
+    value = "FizzBuzz"
+  end
 end
